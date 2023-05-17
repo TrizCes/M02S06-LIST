9732f957689369efd88b9a82711018300ae1d67e
Beatriz Ceschini <biaceschini@gmail.com>
2023-05-16 22:42:05 -0300

    Praticando lógica de programação (Parte 1)

---
+++ Logica de programacao/logic/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    List<int> numbers = new List<int>() { 1, 2, 3, -9 };
+    List<int> newNumbers = new List<int>();
+    for (int i = 0; i < numbers.Count; i++)
+    {
+      int number = numbers[i];
+      newNumbers.Insert(0, number);
+    }
+    for (int j = 0; j < newNumbers.Count; j++)
+    {
+      System.Console.WriteLine(newNumbers[j]);
+    }
+  }
+}
