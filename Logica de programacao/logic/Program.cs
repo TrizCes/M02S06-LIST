4186ab6db83cd21ff5310987ce53410aa6923a8e
Beatriz Ceschini <biaceschini@gmail.com>
2023-05-16 22:52:12 -0300

    Praticando lógica de programação (Parte 1)
    Melhorias

--- Logica de programacao/logic/Program.cs
+++ Logica de programacao/logic/Program.cs
@@ -2,16 +2,28 @@
 {
   private static void Main(string[] args)
   {
-    List<int> numbers = new List<int>() { 1, 2, 3, -9 };
+    List<int> numbers = new List<int>() { 1, 7, 3, -9 };
     List<int> newNumbers = new List<int>();
+
+    Console.WriteLine("Inverter ordem da lista de numeros");
+    Console.Write("Lista original: ");
+    for (int j = 0; j < numbers.Count; j++)
+    {
+      System.Console.Write(numbers[j] + ", ");
+    }
+
     for (int i = 0; i < numbers.Count; i++)
     {
       int number = numbers[i];
       newNumbers.Insert(0, number);
     }
+
+    Console.Write("\nNova ordem: ");
     for (int j = 0; j < newNumbers.Count; j++)
     {
-      System.Console.WriteLine(newNumbers[j]);
+      System.Console.Write(newNumbers[j] + ", ");
     }
+
+    Console.WriteLine("\n\nOrdenando lista de numeros:");
   }
 }
